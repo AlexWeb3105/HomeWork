8228a502441bf530e1490c89781e0b299faceb83
Alexey V <alexweb3105@gmail.com>
2025-09-03 22:57:04 +0500

    РНО в дз

--- DZ-10js/main.js
+++ DZ-10js/main.js
@@ -1,69 +1,77 @@
 //*Первое задание из ДЗ по JS*//
 studentCourse = {
-	alex: {
-		name: 'Alexey',
-		years: '37',
-		region: 'Russia',
-		city: 'Chelyabinsk',
+	name: 'Alexey',
+	years: '37',
+	region: 'Russia',
+	city: 'Chelyabinsk',
+}
+console.log(studentCourse);
+
+//**Создайте метод объекта, который в качестве аргумента будет принимать имя и возвращать строку ‘Hello “переданный аргумент”’.(РНО) */
+const testUsers = {
+	petr: {
+		age: 35,
+		testHello(name) {
+			console.log(`Hello ${name}`);//метод объекта, сюда можно задавть и функцию
+		}
 	}
 }
-console.log(studentCourse.alex);
+testUsers.petr.testHello('Мир')
 
-//**Создайте метод объекта, который в качестве аргумента будет принимать имя и возвращать строку ‘Hello “переданный аргумент”’. */
-test = {
-	name: 'Мир'
-}
-console.log(`Hallo ${test.name}`);
 
 //* Создайте массив объектов с описанием пользователей(такой мы делали в уроке).
 //* Объявите отдельную переменную, в которой будет храниться количество простых пользователей, начальное значение будет - 0. 
 //* Обойдите массив пользователей и если пользователь не является админом - прибавьте к ранее созданной переменной единицу. 
 //*После окончания работы цикла выведите в консоль переменную с количеством простых пользователей. */
 
-//*Тебе нужно знать 1. Как написать массив объектов 2.Как написать цикл 3. Как написать условие if else*/
+//*Тебе нужно знать 1. Как написать массив объектов 2.Как написать цикл 3. Как написать условие if else   (РНО )*/
 
 allUsers = [
 	{
 		name: 'Alex',
 		age: 20,
 		city: 'Moscow',
-		isAdmin: false
+		isAdmin: true,
 	},
 	{
 		name: 'Petr',
 		age: 17,
 		city: 'Magadan',
-		isAdmin: false
+		isAdmin: true,
 	},
 	{
 		name: 'Tom',
 		age: 35,
 		city: 'Altai',
-		isAdmin: true
+		isAdmin: false,
 	},
 	{
 		name: 'Pupi',
 		age: 35,
 		city: 'Altai',
-		isAdmin: false
+		isAdmin: false,
 	},
 	{
 		name: 'Max',
 		age: 35,
 		city: 'Altai',
-		isAdmin: false
+		isAdmin: false,
 	},
 	{
 		name: 'Kate',
 		age: 35,
 		city: 'Altai',
-		isAdmin: true
+		isAdmin: true,
 	}
 ]
+let simpleUser = 0;
+
 for (let i = 0; i < allUsers.length; i++) {
 	if (allUsers[i].isAdmin == true) {
-		console.log(allUsers[i].name + ' Админ');
-	} else {
-		console.log(allUsers[i].name + ' Не админ');
+		simpleUser = simpleUser + 1;
 	}
+
 }
+
+console.log('Простых пользователей ' + simpleUser);
+
