e1b1ab86530a8f1bb6ada2356dc5a14d00c760a1
Alexey V <alexweb3105@gmail.com>
2025-10-05 00:44:46 +0500

    Wake Bake (Header)

---
+++ DZ-11/wake&bake/js/main.js
@@ -0,0 +1,17 @@
+(function () {
+	document.addEventListener('click', burgerInit);
+
+	function burgerInit(e) {
+
+		const burgerIcon = e.target.closest('.burger-top');
+		const burgerNavlink = e.target.closest('.nav__link');
+
+		if (!burgerIcon && !burgerNavlink) return;
+
+		if (!document.body.classList.contains('body--opened-menu')) {
+			document.body.classList.add('body--opened-menu')
+		} else { document.body.classList.remove('body--opened-menu') }
+	}
+
+
+})()
